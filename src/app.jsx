17493bbca0f9aadb129c7906e706e479ca758484
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-07-29 17:53:52 +0200

    Create app skeleton

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <div>Hello, planet</div>;
+
+const documentRoot = document.getElementById("app");
+
+ReactDOM.render(template, documentRoot);
