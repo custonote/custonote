e23e1af73055d5b9fee1fe2f5553c8a6fa18d0b6
Alexever17 <alexever17@gmail.com>
2018-07-29 21:46:16 +0200

    structuring

--- src/app.jsx
+++ src/app.jsx
@@ -3,16 +3,8 @@ import ReactDOM from "react-dom";
 import UIkit from 'uikit';
 //uikit css is included in index.scss
 import './scss/index.scss';
+import App from './components/App';
 
-const testTemplate = <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
-      <div>
-            <div class="uk-card uk-card-primary uk-card-body">
-                  <h3 class="uk-card-title">Hello World</h3>
-                  <p>Test, test 1,2,3 houston do you copy?</p>
-            </div>
-      </div>
-</div>;
-
-const documentRoot = document.getElementById("app");
-
-ReactDOM.render(testTemplate, documentRoot);
+ReactDOM.render(
+      <App />, document.getElementById('app')
+);
