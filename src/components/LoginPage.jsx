95063cf8a5270bfa8041d2dcbbde2a45dd5e4d0e
Søren Gravesen <grav@hesehus.dk>
2018-08-01 13:16:10 +0200

    Added content to LoginPage component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 
 class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handleSubmit')
+    }
+
     render() {
         return (
-            <div>
-                <h3>Login Page</h3>
+            <div className='LoginPage'>
+                <div className='login-container'>
+                    <div className='cover'>
+                        <h3><span>Login</span><span>Custonote</span></h3>
+                    </div>
+                    <div className='social-bar'>
+                        <div className='login login-facebook'>
+                            <p>Login with Facebook</p>
+                        </div>
+                        <div className='login login-google'>
+                            <p>Login with Google</p>
+                        </div>
+                        <div className='login login-github'>
+                            <p>Login with Github</p>
+                        </div>
+                    </div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type='text' placeholder='Username' />
+                        <input type='password' placeholder='Password' />
+                        <button>Login</button>
+                    </form>
+                </div>
             </div>
         )
     }
