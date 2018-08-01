7f154d5a59123a92b4ed1329a973126f0df4de35
Søren Gravesen <grav@hesehus.dk>
2018-08-01 13:27:44 +0200

    Modified handleSubmit on LoginPage.jsx

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -12,9 +12,30 @@ class LoginPage extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('handleSubmit')
+    // Handle login (Awaiting backend)
+    handleSubmit = (target, e) => {
+        switch(target) {
+            case 'form':
+                console.log('FORM LOGIN');
+                e.preventDefault();
+                // Form login code here
+                break;
+            case 'fb':
+                console.log('LOGIN FACEBOOK');
+                // Facebook login code here
+                break;
+            case 'google':
+                console.log('GOOGLE LOGIN');
+                // Google login code here
+                break;
+            case 'github':
+                console.log('GITHUB LOGIN');
+                // Github login code here
+                break;
+            default:
+                console.log('Sorry, your login request was not recognized.');
+                break;
+        }
     }
 
     render() {
@@ -25,17 +46,17 @@ class LoginPage extends React.Component {
                         <h3><span>Login</span><span>Custonote</span></h3>
                     </div>
                     <div className='social-bar'>
-                        <div className='login login-facebook'>
+                        <div className='login login-facebook' onClick={() => this.handleSubmit('fb')}>
                             <p>Login with Facebook</p>
                         </div>
-                        <div className='login login-google'>
+                        <div className='login login-google' onClick={() => this.handleSubmit('google')}>
                             <p>Login with Google</p>
                         </div>
-                        <div className='login login-github'>
+                        <div className='login login-github' onClick={() => this.handleSubmit('github')}>
                             <p>Login with Github</p>
                         </div>
                     </div>
-                    <form onSubmit={this.handleSubmit}>
+                    <form onSubmit={(e) => this.handleSubmit('form', e)}>
                         <input type='text' placeholder='Username' />
                         <input type='password' placeholder='Password' />
                         <button>Login</button>
