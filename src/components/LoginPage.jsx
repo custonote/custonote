85598554358e9fb9ac83893f91bef7b9bde80b3f
Søren Gravesen <grav@hesehus.dk>
2018-08-01 10:54:57 +0200

    Temporarily removed UIKit classes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,12 +3,8 @@ import React from 'react'
 class LoginPage extends React.Component {
     render() {
         return (
-            <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid="true">
-                <div>
-                    <div className="uk-card uk-card-primary uk-card-body">
-                        <h3 className="uk-card-title">Login Page</h3>
-                    </div>
-                </div>
+            <div>
+                <h3>Login Page</h3>
             </div>
         )
     }
