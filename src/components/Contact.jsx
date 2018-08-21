10bc92a5135b22ab39de3c9317420280887404c5
Natalie Roode <nroode@gmail.com>
2018-08-21 00:26:01 -0700

    fixed icons; added responsive elements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,18 +5,20 @@ class Contact extends React.Component {
         return (
             <div>
                 <section id="contact">
-                    <h1>Contact</h1>
+                    <h1 id="contact__title">Contact</h1>
 
-                    <div className="contact__cards" uk-grid="true">
+                    <div className="contact__cards uk-grid-match" uk-grid="true">
 
-                        <div className="uk-card contact__card--left uk-width-1-2">
-                            <h2>We want to help!</h2>
-                            <a href="#" uk-icon="icon: mail" className="uk-icon-button"></a>
-                            <a href="https://github.com/custonote/custonote/tree/master" uk-icon="icon: github, ratio: 2" class="uk-icon-button"></a>
+                        <div className="uk-card contact__card--left uk-width-1-2@s">
+                            <h2 className="card__heading">We want to help you!</h2>
+                            <div className="contact__icons uk-flex uk-flex-center">
+                                <a href="#" uk-icon="icon: mail; ratio: 4" className="uk-icon-link contact__icon"></a>
+                                <a href="https://github.com/custonote/custonote/tree/master" uk-icon="icon: github; ratio: 4" className="contact__icon"></a>
+                            </div>
                         </div>
 
-                        <div className="uk-card contact__card--right uk-width-1-2">
-                            <img src="https://images.unsplash.com/photo-1466096115517-bceecbfb6fde?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=427bcc1d8e2505d31a239d0de6b13f75&auto=format&fit=crop&w=1650&q=80"></img>
+                        <div className="uk-card contact__card--right uk-width-1-2@s">
+                            
                         </div>
                     </div>
                 </section>
