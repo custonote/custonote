761c3bdeda01cf3983c4a658a0a0b893f09d1512
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-09-02 23:31:13 +0200

    Load UI Kit icons

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+UIkit.use(Icons);
+
 //uikit css is included in index.scss
 import './scss/index.scss';
 import App from './components/App';
 
-ReactDOM.render(
-      <App />, document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
